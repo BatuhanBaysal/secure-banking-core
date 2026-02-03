e85c923b8ba20eed8160ddfde16d9495692adab1
Batuhan Baysal <batuhanbaysal3@gmail.com>
2026-02-03 10:29:40 +0300

    Create AgeValidator.java

---
+++ backend/src/main/java/com/batuhan/banking_service/validator/AgeValidator.java
@@ -0,0 +1,22 @@
+package com.batuhan.banking_service.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+import java.time.Period;
+
+public class AgeValidator implements ConstraintValidator<MinAge, LocalDate> {
+
+    private int minAge;
+
+    @Override
+    public void initialize(MinAge constraintAnnotation) {
+        this.minAge = constraintAnnotation.value();
+    }
+
+    @Override
+    public boolean isValid(LocalDate birthDate, ConstraintValidatorContext context) {
+        if (birthDate == null) return false;
+        return Period.between(birthDate, LocalDate.now()).getYears() >= minAge;
+    }
+}
