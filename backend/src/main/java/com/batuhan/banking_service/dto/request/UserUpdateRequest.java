b7c355ef6221eb0acfaa1513f50fc6116393a254
Batuhan Baysal <batuhanbaysal3@gmail.com>
2026-02-04 10:32:47 +0300

    Create UserUpdateRequest.java

---
+++ backend/src/main/java/com/batuhan/banking_service/dto/request/UserUpdateRequest.java
@@ -0,0 +1,32 @@
+package com.batuhan.banking_service.dto.request;
+
+import com.batuhan.banking_service.dto.common.AddressDto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserUpdateRequest {
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotNull(message = "Address information is required")
+    @Valid
+    private AddressDto address;
+}
