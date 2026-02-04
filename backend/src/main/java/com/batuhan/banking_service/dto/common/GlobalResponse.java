98b5c7cc5be1e6ac6c3021dadd3d2a089c3f917a
Batuhan Baysal <batuhanbaysal3@gmail.com>
2026-02-04 10:27:23 +0300

    Create GlobalResponse.java

---
+++ backend/src/main/java/com/batuhan/banking_service/dto/common/GlobalResponse.java
@@ -0,0 +1,40 @@
+package com.batuhan.banking_service.dto.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GlobalResponse<T> {
+
+    private boolean success;
+    private String message;
+    private T data;
+
+    public static <T> GlobalResponse<T> success(T data, String message) {
+        return GlobalResponse.<T>builder()
+                .success(true)
+                .message(message)
+                .data(data)
+                .build();
+    }
+
+    public static <T> GlobalResponse<T> error(String message) {
+        return GlobalResponse.<T>builder()
+                .success(false)
+                .message(message)
+                .build();
+    }
+
+    public static <T> GlobalResponse<T> error(String message, T data) {
+        return GlobalResponse.<T>builder()
+                .success(false)
+                .message(message)
+                .data(data)
+                .build();
+    }
+}
