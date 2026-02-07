9c55d381476c8a8b7de794092580354afe0dc507
Batuhan Baysal <batuhanbaysal3@gmail.com>
2026-02-07 12:44:18 +0300

    Create EmailServiceImpl.java

---
+++ backend/src/main/java/com/batuhan/banking_service/service/impl/EmailServiceImpl.java
@@ -0,0 +1,53 @@
+package com.batuhan.banking_service.service.impl;
+
+import com.batuhan.banking_service.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender mailSender;
+    private static final String FROM_EMAIL = "no-reply@batuhanbanking.com";
+    private static final String SUBJECT_PREFIX = "Transfer Notification - Ref: ";
+
+    @Override
+    @Async
+    public void sendTransferEmail(String toEmail, String name, BigDecimal amount, String receiverIban, String referenceNumber) {
+        log.info("Preparing to send notification email for Ref: {}", referenceNumber);
+
+        try {
+            SimpleMailMessage message = prepareTransferMessage(toEmail, name, amount, receiverIban, referenceNumber);
+            mailSender.send(message);
+            log.info("Notification email sent successfully for Ref: {}", referenceNumber);
+        } catch (Exception e) {
+            log.error("Error occurred while sending email for Ref {}: {}", referenceNumber, e.getMessage());
+        }
+    }
+
+    private SimpleMailMessage prepareTransferMessage(String toEmail, String name, BigDecimal amount, String receiverIban, String referenceNumber) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(FROM_EMAIL);
+        message.setTo(toEmail);
+        message.setSubject(SUBJECT_PREFIX + referenceNumber);
+        message.setText(buildTransferEmailBody(name, amount, receiverIban, referenceNumber));
+        return message;
+    }
+
+    private String buildTransferEmailBody(String name, BigDecimal amount, String receiverIban, String referenceNumber) {
+        return String.format(
+                "Dear %s,\n\nA transfer of %s TRY has been successfully made to IBAN %s.\n\n" +
+                        "Reference Number: %s\n\n" +
+                        "You can use this reference number for your inquiries. Have a nice day.",
+                name, amount, receiverIban, referenceNumber
+        );
+    }
+}
