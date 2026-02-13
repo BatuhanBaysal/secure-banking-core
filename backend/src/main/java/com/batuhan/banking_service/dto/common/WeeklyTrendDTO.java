139ba7340816a838eefb861c2746de7a9687e5eb
Batuhan Baysal <batuhanbaysal3@gmail.com>
2026-02-13 13:13:52 +0300

    refactor: convert WeeklyTrendDTO to record

--- backend/src/main/java/com/batuhan/banking_service/dto/common/WeeklyTrendDTO.java
+++ backend/src/main/java/com/batuhan/banking_service/dto/common/WeeklyTrendDTO.java
@@ -1,18 +1,9 @@
 package com.batuhan.banking_service.dto.common;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class WeeklyTrendDTO {
-
-    private LocalDateTime date;
-    private BigDecimal totalAmount;
-}
+public record WeeklyTrendDTO(
+        LocalDateTime date,
+        BigDecimal totalAmount
+) {}
