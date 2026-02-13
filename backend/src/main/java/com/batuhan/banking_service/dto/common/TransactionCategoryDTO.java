8f835542bfb536b08f3a2ceb4ca75bd5ad63b500
Batuhan Baysal <batuhanbaysal3@gmail.com>
2026-02-13 13:12:26 +0300

    feat: add TransactionCategoryDTO for expense categorization

--- backend/src/main/java/com/batuhan/banking_service/dto/common/TransactionCategoryDTO.java
+++ backend/src/main/java/com/batuhan/banking_service/dto/common/TransactionCategoryDTO.java
@@ -1,19 +1,9 @@
 package com.batuhan.banking_service.dto.common;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class TransactionCategoryDTO {
-
-    private String category;
-    private BigDecimal amount;
-    private Double percentage;
-}
+public record TransactionCategoryDTO(
+        String category,
+        BigDecimal amount,
+        Double percentage
+) {}
