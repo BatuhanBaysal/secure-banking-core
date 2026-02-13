b9ca4efa657a867fa3fc9c99f8c72a29aef210e3
Batuhan Baysal <batuhanbaysal3@gmail.com>
2026-02-13 13:10:32 +0300

    refactor: simplify AddressDto using Java Record for immutability

--- backend/src/main/java/com/batuhan/banking_service/dto/common/AddressDto.java
+++ backend/src/main/java/com/batuhan/banking_service/dto/common/AddressDto.java
@@ -1,26 +1,10 @@
 package com.batuhan.banking_service.dto.common;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class AddressDto {
-
-    @NotBlank(message = "City is required")
-    private String city;
-
-    @NotBlank(message = "Street is required")
-    private String street;
-
-    @NotBlank(message = "Zip code is required")
-    private String zipCode;
-
-    @NotBlank(message = "Phone number is required")
-    private String phoneNumber;
-}
+public record AddressDto(
+        @NotBlank(message = "City is required") String city,
+        @NotBlank(message = "Street is required") String street,
+        @NotBlank(message = "Zip code is required") String zipCode,
+        @NotBlank(message = "Phone number is required") String phoneNumber
+) {}
