59655c5a060480d16cf9be34167e15ff504fc2be
Batuhan Baysal <batuhanbaysal3@gmail.com>
2026-02-13 13:17:32 +0300

    refactor: convert AccountResponse to record

--- backend/src/main/java/com/batuhan/banking_service/dto/response/AccountResponse.java
+++ backend/src/main/java/com/batuhan/banking_service/dto/response/AccountResponse.java
@@ -2,22 +2,12 @@ package com.batuhan.banking_service.dto.response;
 
 import com.batuhan.banking_service.entity.enums.AccountStatus;
 import com.batuhan.banking_service.entity.enums.CurrencyType;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class AccountResponse {
-
-    private Long id;
-    private String iban;
-    private BigDecimal balance;
-    private CurrencyType currency;
-    private AccountStatus status;
-    private String customerNumber;
-}
+public record AccountResponse(
+        String iban,
+        BigDecimal balance,
+        CurrencyType currency,
+        AccountStatus status,
+        String customerNumber
+) {}
