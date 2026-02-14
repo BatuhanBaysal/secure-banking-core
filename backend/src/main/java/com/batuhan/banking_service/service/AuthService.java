5b53891372f0b34b0ef2ae5e888c5742f4ebed71
Batuhan Baysal <batuhanbaysal3@gmail.com>
2026-02-14 13:26:51 +0300

    chore: remove custom security implementation in favor of Keycloak

--- backend/src/main/java/com/batuhan/banking_service/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.batuhan.banking_service.service;
-
-import com.batuhan.banking_service.dto.request.AuthRequest;
-import com.batuhan.banking_service.dto.response.AuthResponse;
-
-public interface AuthService {
-
-    AuthResponse authenticate(AuthRequest request);
-}
